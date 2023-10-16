c748558b41b16208b158ab13308cb03e2fcdb224
Celia Lucía Castañeda Arizaga <chuggaaconroy888@gmail.com>
2023-10-16 09:40:35 -0600

    Added Theme Change
    When onClick on Profile img

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { ThemeProvider } from "@mui/material/styles";
 import { THEME } from "./ui/Lucy/theme";
+import {THEME2} from "./ui/Arturo/theme";
 import Gallery from "./components/gallery/Gallery";
 import Header from "./components/header/Header";
 import { Container } from "@mui/material";
@@ -12,14 +13,41 @@ import { Container } from "@mui/material";
 
 function App() {
   const [backgroundImage, setBackgroundImage] = useState(null); // State for background image
+  const [pTheme, setPTheme] = useState(null); // State for background image
 
   // Function to set the background image when a different item is clicked
   const setBackgroundFromItem = (item) => {
     setBackgroundImage(item.img);
   };
 
+  const changeTheme = (selectedPerson) => {
+    setPTheme(selectedPerson);
+    console.log(selectedPerson);
+  };
+  
   return (
-    <ThemeProvider theme={THEME}>
+    <>
+    {pTheme === "Arturo"? (
+      <ThemeProvider theme={THEME2}>
+        <div
+          className="App"
+          style={{
+            backgroundImage: `url(${backgroundImage})`,
+            backgroundRepeat: "repeat",
+            backgroundSize: "500px",
+          }}
+        >
+          <Container
+            maxWidth="xl"
+            style={{ background: "linear-gradient(to bottom, #FFC0CB, #E0115F" }}
+          >
+            <Header onPersonSelect={changeTheme} />
+            <Gallery onItemSelect={setBackgroundFromItem} />
+          </Container>
+        </div>
+      </ThemeProvider>
+    ):(
+      <ThemeProvider theme={THEME}>
       <div
         className="App"
         style={{
@@ -32,11 +60,13 @@ function App() {
           maxWidth="xl"
           style={{ background: "linear-gradient(to bottom, #FFC0CB, #E0115F" }}
         >
-          <Header />
+          <Header onPersonSelect={changeTheme} />
           <Gallery onItemSelect={setBackgroundFromItem} />
         </Container>
       </div>
     </ThemeProvider>
+    )}
+  </>
   );
 }
 
