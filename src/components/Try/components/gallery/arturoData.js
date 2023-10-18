c81aae30c4cafb2df1db229fecfb06d78f521db6
Slothfulsilver <arturo_garciagb@hotmail.com>
2023-10-18 13:54:35 -0600

    Arturo Gallery

---
+++ src/components/Try/components/gallery/arturoData.js
@@ -0,0 +1,75 @@
+import d1 from "../../ui/Arturo/media/Dibujo1.jpg";
+import d2 from "../../ui/Arturo/media/Dibujo2.jpg";
+import d3 from "../../ui/Arturo/media/Dibujo3.jpg";
+import d4 from "../../ui/Arturo/media/Dibujo4.jpg";
+import d5 from "../../ui/Arturo/media/Dibujo5.jpg";
+import d6 from "../../ui/Arturo/media/Dibujo6.jpg";
+import d7 from "../../ui/Arturo/media/Dibujo7.jpg";
+import b from "../../ui/Arturo/media/Bear.gif";
+
+export const itemData = [
+  {
+    img: d1,
+    title: "Coup d'état",
+    subtitle: "Digital Art",
+    date: "Sept 2021",
+    description:
+      "Work for a Scene Design course",
+  },
+  {
+    img: d2,
+    title: "Throne",
+    subtitle: "Digital Art",
+    date: "Sept 2022",
+    description:
+      "Anatomy and color study",
+  },
+  {
+    img: d3,
+    title: "Dress",
+    subtitle: "Digital Art",
+    date: "Nov 2022",
+    description:
+    "Anatomy and clothes study"      
+  },
+  {
+    img: d4,
+    title: "Posing Girl 1",
+    subtitle: "Digital Art",
+    date: "Nov 2022",
+    description:
+      "Anatomy study",
+  },
+  {
+    img: d5,
+    title: "Black & White",
+    subtitle: "Digital Art",
+    date: "Sept 2022",
+    description:
+      "Anatomy and clothes study",
+  },
+  {
+    img: d6,
+    title: "Somnium Night",
+    subtitle: "Duck McDuck",
+    date: "Sept 2022",
+    description:
+      "Character Design course classwork",
+  },
+  {
+    img: d7,
+    title: "Mansion",
+    subtitle: "Digital Art",
+    date: "Oct 2021",
+    description:
+      "Scene Design classwork. Made in collaboration of Carlos 'Kirl' Villaseñor Rabago ",
+  },
+  {
+    img: b,
+    title: "Bear",
+    subtitle: "Digital Art",
+    date: "Sept 2022",
+    description:
+      'Charater Design course midterm exam',
+  },
+];
